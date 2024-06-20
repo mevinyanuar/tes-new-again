94edca6ebd167ff1ae20ee4399496c2db4b680c0
Mevin <mmevinalifviyanuar@gmail.com>
2024-06-20 14:14:14 +0700

    first commit

---
+++ tesirr.js
@@ -0,0 +1,8 @@
+let nama = 'David'
+let peran = 'Penyihirs'
+
+let sentence = peran === 'Ksatria' ? `Selamat datang di Dunia Proxytia, ${nama}, 
+Halo ${peran} ${nama}, kamu dapat menyerang dengan senjatamu!` : peran === 'Tabib' ? `Selamat datang di Dunia Proxytia, ${nama}, 
+Halo ${peran} ${nama}, kamu dapat menyembuhkan temanmu!`: peran === 'Penyihir' ? `Selamat datang di Dunia Proxytia, ${nama}, 
+Halo ${peran} ${nama}, ciptakan keajaiban yang membantumu!` : `nama dan peran harus diisi`
+console.log(sentence)
